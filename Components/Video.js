6b0fbe14c2be4d0453a83b91ffc030b36e2dac02
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-02-01 23:06:58 +0530

    Video.js

--- Components/Video.js
+++ Components/Video.js
@@ -2,6 +2,7 @@ function Video(){
     return (
         <div> 
             <img src= "https://loremflickr.com/640/360" alt ="This is an dummy image" />
+            <div className="" style={{backgroundColor: 'red'}}>{topic}</div>
         </div>
     );
 }
