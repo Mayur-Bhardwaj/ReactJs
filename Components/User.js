d2a9432c9496dfe169ce4348ee14a55a452c6b2d
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-02-01 22:40:17 +0530

    User.js

--- Components/User.js
+++ Components/User.js
@@ -6,7 +6,18 @@ function User()
         <>
         <h1>User Or Functional Component.</h1>
         <h2>This is written when we use jsx.</h2>
+        <Demo />
         </>
-        );
+    );
 }
+
+function Demo()
+{
+    return(
+        <>
+        <h1>This is our Demo Component.</h1>
+        </>
+    );
+}
+
 export default User;
