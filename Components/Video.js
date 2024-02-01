06175154f635d23ef1d705b1872102d414f90312
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-02-01 23:00:04 +0530

    Add files via upload

---
+++ Components/Video.js
@@ -0,0 +1,9 @@
+function Video(){
+    return (
+        <div> 
+            <img src= "https://loremflickr.com/640/360" alt ="This is an dummy image" />
+        </div>
+    );
+}
+
+export default Video;
