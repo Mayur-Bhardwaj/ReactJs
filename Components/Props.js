d983c3b26e392bb259429fd885a382cc496a7a72
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-02-02 19:40:16 +0530

    Props

---
+++ Components/Props.js
@@ -0,0 +1,18 @@
+function Props(prop){
+    return(
+        <div>
+            <h1>{prop.title}</h1>
+        </div>
+    );
+}
+
+export default Props;
+// function Properties({title}){
+//     return(
+//         <div>
+//         <img src= "https://loremflickr.com/640/360" alt ="This is an dummy image" />
+//         <div className="" style={{backgroundColor: 'red'}}>{title}</div>
+//     </div>
+//     );
+// }
+// export default Properties;
