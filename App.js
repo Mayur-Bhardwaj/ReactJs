595027420b3222a5a1cec5fa7bbb0609b0a4ad37
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-02-21 11:20:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import ClassComponent from "./Components/ClassComponent";
+import {Functional} from "./Components/Functional.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mayur Bhardwaj</h1>
+     <ClassComponent /> 
+     <Functional />
+    </div>
+  );
+}
+
+export default App;
