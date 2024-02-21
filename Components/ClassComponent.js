107b628abe3f12b75e2497bf71bc8ce9565d1410
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-02-21 11:19:33 +0530

    Add files via upload

--- Components/ClassComponent.js
+++ Components/ClassComponent.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-class ClassComponent extends Component{
-    render()
-    {
-        return(
-            <h1>This is our Class Component.</h1>
-        );
+// class ClassComponent extends Component{
+//     render(){
+//         return(
+//             <div>
+//             <h1>Hello !!</h1>
+//             <h1>This is our Class Component.</h1>
+//             </div>
+//         )
+//     }
+// }
+// export default ClassComponent;
 
+
+export default class ClassComponent extends Component{
+    render(){
+        return(
+            <div>
+            <h1>Hello !!</h1>
+            <h1>This is our Class Component.</h1>
+            </div>
+        )
     }
 }
-export default ClassComponent;
---
+++ Components/Functional.js
@@ -0,0 +1,16 @@
+// function Functional(){
+//     return(
+//         <h1>This is Functional Component.</h1>
+//     )
+// }
+
+// export default Functional;
+
+export function Functional() {
+    return(
+        <div>
+        <h1>Functional Component.</h1>
+        <h1>Another Way of Functional Component.</h1>
+        </div>
+    );
+}
